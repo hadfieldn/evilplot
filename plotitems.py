8c30ce7841580ff829f25d90d2660340277387f8
Andrew McNabb <amcnabb@mcnabbs.org>
2008-05-23 17:29:56 -0600

    added license information

--- plotitems.py
+++ plotitems.py
@@ -1,4 +1,26 @@
-#!/usr/bin/env python
+# EvilPlot
+# Copyright 2008 Brigham Young University
+#
+# This file is part of EvilPlot.
+#
+# EvilPlot is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# EvilPlot is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# EvilPlot.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Inquiries regarding any further use of the Materials contained on this site,
+# please contact the Copyright Licensing Office, Brigham Young University,
+# 3760 HBLL, Provo, UT 84602, (801) 422-9339 or 422-3821, e-mail
+# copyright@byu.edu.
+
 """Individual items that can be plotted."""
 
 from __future__ import division
